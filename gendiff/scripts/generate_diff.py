093182f505e258836b1dfa926613746768b416ac
Nella <nella611@yandex.ru>
2021-11-23 17:04:43 +0200

    added is_json and is_yaml to generate_diff

--- gendiff/scripts/generate_diff.py
+++ gendiff/scripts/generate_diff.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
 import json
+import yaml
+
+
+def is_json(path: str) -> bool:
+    return path[-5:] == '.json'
+
+
+def is_yaml(path: str) -> bool:
+    return path[-4:] in ('yaml', '.yml')
+
+
+def read_file(path: str):
+    if is_json(path):
+        return json.load(open(path))
+    if is_yaml(path):
+        return yaml.safe_load(open(path))
 
 
 def generate_diff(first_file, second_file) -> str:
-    first_file = json.load(open(first_file))
-    second_file = json.load(open(second_file))
+    first_file = read_file(first_file)
+    second_file = read_file(second_file)
     accum_keys = first_file.copy()
     accum_keys.update(second_file)
     accum_keys = [el for el in accum_keys]
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 import os
-from pathlib import Path
 from gendiff import generate_diff
 
 
 dirname = os.path.dirname(__file__)
-first_file = os.path.join(dirname, 'fixtures/file1.json')
-second_file = os.path.join(dirname, 'fixtures/file2.json')
-print(first_file)
+first_json_file = os.path.join(dirname, 'fixtures/file1.json')
+second_json_file = os.path.join(dirname, 'fixtures/file2.json')
 
 
-def test_generate_diff():
-    answer = open(os.path.join(dirname, 'fixtures/answer_true'))
-    assert generate_diff(first_file, second_file) == answer.read()
+def test_generate_diff_json():
+    answer = open(os.path.join(dirname, 'fixtures/answer'))
+    assert generate_diff(first_json_file, second_json_file) == answer.read()
+
+
+first_yaml_file = os.path.join(dirname, 'fixtures/file1.yml')
+second_yaml_file = os.path.join(dirname, 'fixtures/file2.yaml')
+
+
+def test_generate_diff_yaml():
+    answer = open(os.path.join(dirname, 'fixtures/answer'))
+    assert generate_diff(first_yaml_file, second_yaml_file) == answer.read()
