b38643c6acacacff213badf14e98bcff2a0d3e2a
Nella <nella611@yandex.ru>
2021-12-09 13:39:54 +0200

    changed stylish.py

--- gendiff/scripts/format_output/stylish.py
+++ gendiff/scripts/format_output/stylish.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 
+
+def type_of_value(value):
+    if value is False:
+        result = 'false'
+    elif value is True:
+        result = 'true'
+    elif value is None:
+        result = 'null'
+    elif not value:
+        result = None
+    else:
+        result = value
+    return result
+
+
 def stylish(dict_, spaces_count=0):  # noqa: <error code>
     result = '{\n'
     sorted_keys = sorted(dict_.keys(),
@@ -10,30 +25,20 @@ def stylish(dict_, spaces_count=0):  # noqa: <error code>
                 result += f'{(spaces_count + 2) * " "}{key}: ' \
                           f'{stylish(dict_[key], spaces_count + 4)}\n'
             else:
-                if dict_[key] is False:
-                    result += f'{(spaces_count + 2) * " "}{key}: false\n'
-                elif dict_[key] is True:
-                    result += f'{(spaces_count + 2) * " "}{key}: true\n'
-                elif dict_[key] is None:
-                    result += f'{(spaces_count + 2) * " "}{key}: null\n'
-                elif not dict_[key]:
-                    result += f'{(spaces_count + 2) * " "}{key}:\n'
+                value = type_of_value(dict_[key])
+                if value:
+                    result += f'{(spaces_count + 2) * " "}{key}: {value}\n'
                 else:
-                    result += f'{(spaces_count + 2) * " "}{key}: {dict_[key]}\n'
+                    result += f'{(spaces_count + 2) * " "}{key}:\n'
         else:
             if isinstance(dict_[key], dict):
                 result += f'{(spaces_count + 4) * " "}{key}: ' \
                           f'{stylish(dict_[key], spaces_count + 4)}\n'
             else:
-                if dict_[key] is False:
-                    result += f'{(spaces_count + 4) * " "}{key}: false\n'
-                elif dict_[key] is True:
-                    result += f'{(spaces_count + 4) * " "}{key}: true\n'
-                elif dict_[key] is None:
-                    result += f'{(spaces_count + 4) * " "}{key}: null\n'
-                elif not dict_[key]:
-                    result += f'{(spaces_count + 4) * " "}{key}:\n'
+                value = type_of_value(dict_[key])
+                if value:
+                    result += f'{(spaces_count + 4) * " "}{key}: {value}\n'
                 else:
-                    result += f'{(spaces_count + 4) * " "}{key}: {dict_[key]}\n'
+                    result += f'{(spaces_count + 4) * " "}{key}:\n'
     result += ' ' * spaces_count + '}'
     return result
