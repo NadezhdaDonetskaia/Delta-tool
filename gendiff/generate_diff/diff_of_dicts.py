941c0c169e0e056f977fbc699bfc0a0a23fd3d5b
Nella <nella611@yandex.ru>
2022-01-10 18:06:49 +0200

    added 'type': 'nested' in diff_of_dicts

--- gendiff/generate_diff/diff_of_dicts.py
+++ gendiff/generate_diff/diff_of_dicts.py
@@ -5,39 +5,30 @@ def diff_of_dicts(dict1, dict2):
     result = dict()
     all_keys = set(list(dict1.keys()) + list(dict2.keys()))
     for key in all_keys:
-        # про уровни вложенности крутила всяко, не вижу,
-        # как можно сделать в один
-        # по логике ключ в обоих словарях - значит другой иф на равность
-        # можно сделать без второго вложенного иф,
-        # но проверки будут двойными и длинными
-        # (типо, входит в один, но не входит в другой) - как-то это не красиво((
-        if key in dict1 and key in dict2:
-            if dict1[key] == dict2[key]:
-                result[key] = {
-                    'value': dict1[key],
-                    'type': 'unchanged',
-                }
-            elif isinstance(dict1[key], dict) \
-                    and isinstance(dict2[key], dict):
-                # на мой взгляд этот случай отличется:
-                # если они не равны, то мы еще не знаем,
-                # где именно -> надо идти вглубь
-                # зачем нам добавлять сюда ключи типа,
-                # если при распаковке мы всё равно будем идти ниже?
-                result[key] = diff_of_dicts(dict1[key], dict2[key])
-            else:
-                result[key] = {
-                    'value': (dict1[key], dict2[key]),
-                    'type': 'changed',
-                }
-        elif key in dict1:
+        if key not in dict2:
             result[key] = {
                 'value': dict1[key],
                 'type': 'deleted',
             }
-        else:
+        elif key not in dict1:
             result[key] = {
                 'value': dict2[key],
                 'type': 'added',
+            }
+        if dict1[key] == dict2[key]:
+            result[key] = {
+                'value': dict1[key],
+                'type': 'unchanged',
+            }
+        elif isinstance(dict1[key], dict) \
+                and isinstance(dict2[key], dict):
+            result[key] = {
+                'value': diff_of_dicts(dict1[key], dict2[key]),
+                'type': 'nested',
+            }
+        else:
+            result[key] = {
+                'value': (dict1[key], dict2[key]),
+                'type': 'changed',
             }
     return result
