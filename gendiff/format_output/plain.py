a04489bb864ba9e1f5f063e2f249e376d9887bfa
Nella <nella611@yandex.ru>
2022-01-11 15:47:53 +0200

    changed plain.py (transform_value)

--- gendiff/format_output/plain.py
+++ gendiff/format_output/plain.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import json
 
-# change name
-def type_of_value(value):
+
+def transform_value(value):
     if isinstance(value, (list, tuple, dict, set)):
         result_value = '[complex value]'
     elif isinstance(value, bool) or value is None:
@@ -14,26 +14,33 @@ def type_of_value(value):
     return result_value
 
 
+def formation_value(value, spaces):
+    pass
+
+
 def plain(dict_, path='', child=False):
     result = ''
     sorted_keys = sorted(dict_.keys())
     for key in sorted_keys:
         if dict_[key].get('type') == 'changed':
-            value_del = type_of_value(dict_[key].get('value')[0])
-            value_add = type_of_value(dict_[key].get('value')[1])
+            value_del = dict_[key].get('value')[0]
+            value_del = transform_value(value_del)
+            value_add = dict_[key].get('value')[1]
+            value_add = transform_value(value_add)
             result += f"Property '{path + key}' was updated. " \
                       f"From {value_del} to {value_add}\n"
         elif dict_[key].get('type') == 'deleted':
             result += f"Property '{path + key}' was removed\n"
         elif dict_[key].get('type') == 'added':
-            value_add = type_of_value(dict_[key].get('value'))
+            value_add = dict_[key].get('value')
+            value_add = transform_value(value_add)
             result += f"Property '{path + key}' " \
                       f"was added with value: {value_add}\n"
         elif dict_[key].get('type') == 'unchanged':
-            if isinstance(dict_[key].get('value'), dict):
-                result += plain(dict_[key].get('value'), path=path + f'{key}.', child=True)
+            pass
         else:
-            result += plain(dict_[key], path=path + f'{key}.', child=True)
+            new_value = dict_[key].get('value')
+            result += plain(new_value, path=path + f'{key}.', child=True)
     if not child:
         result = result[:-1]
     return result
