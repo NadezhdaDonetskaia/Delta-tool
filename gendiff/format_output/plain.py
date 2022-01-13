e9f236119ad95f6d0e555b493ac96467db8e2b02
Nella <nella611@yandex.ru>
2022-01-13 14:41:21 +0200

    added plain_deep in plain

--- gendiff/format_output/plain.py
+++ gendiff/format_output/plain.py
@@ -25,26 +25,30 @@ def get_report_diff(path, type):
     return report
 
 
-def plain(dict_, path='', child=False):  # noqa: C901
+def plain_deep(dict_level, path='', child=False):  # noqa: C901
     result = ''
-    sorted_keys = sorted(dict_.keys())
+    sorted_keys = sorted(dict_level.keys())
     for key in sorted_keys:
-        type = dict_[key].get('type')
+        type = dict_level[key].get('type')
         if type == 'unchanged':
             continue
-        if dict_[key].get('type') == 'nested':
-            new_value = dict_[key].get('value')
-            result += plain(new_value, path=f'{path + key}.', child=True)
+        if dict_level[key].get('type') == 'nested':
+            new_value = dict_level[key].get('value')
+            result += plain_deep(new_value, path=f'{path + key}.', child=True)
             continue
         result += get_report_diff(f'{path + key}', type)
         if type == 'changed':
-            value_del, value_add = dict_[key].get('value')
+            value_del, value_add = dict_level[key].get('value')
             value_del = stringify(value_del)
             value_add = stringify(value_add)
             result += f'From {value_del} to {value_add}\n'
         elif type == 'added':
-            value_add = stringify(dict_[key].get('value'))
+            value_add = stringify(dict_level[key].get('value'))
             result += f'{value_add}\n'
     if not child:
         result = result[:-1]
     return result
+
+
+def plain(dict_):
+    return plain_deep(dict_, path='')
