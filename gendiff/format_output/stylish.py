ed444a894ac4a8213f52a639a5375157de2c4132
Nella <nella611@yandex.ru>
2022-01-13 14:08:30 +0200

    added stylish_deep to stylish

--- gendiff/format_output/stylish.py
+++ gendiff/format_output/stylish.py
@@ -10,15 +10,15 @@ def transform_value(value):
     return result
 
 
-def formation_value(value, spaces):
+def formation_value(value, deep):
     if not isinstance(value, dict):
         return transform_value(value)
     keys_ = sorted(value.keys())
     result_value = '{\n'
     for key in keys_:
-        val = formation_value(value[key], spaces + 4)
-        result_value += f'{" " * (spaces + 4)}{key}: {val}\n'
-    result_value += " " * spaces + '}'
+        val = formation_value(value[key], deep + 1)
+        result_value += f'{" " * (deep * 4)}{key}: {val}\n'
+    result_value += " " * (deep - 1) * 4 + '}'
     return result_value
 
 
@@ -33,30 +33,34 @@ def transform_key(type, key):
         return '+ ' + str(key)
 
 
-def stylish(dict_, spaces=0):
+def stylish_deep(dict_level, deep):
     result = '{\n'
-    sorted_keys = sorted(dict_.keys())
+    sorted_keys = sorted(dict_level.keys())
     for key in sorted_keys:
-        if not isinstance(dict_[key], dict):
-            value = transform_value(dict_[key])
-            result += f'{" " * (spaces + 4)}{key}: {value}\n'
-        elif dict_[key].get('type') == 'nested':
-            val = stylish(dict_[key].get('value'), spaces + 4)
+        if not isinstance(dict_level[key], dict):
+            value = transform_value(dict_level[key])
+            result += f'{" " * (deep * 4)}{key}: {value}\n'
+        elif dict_level[key].get('type') == 'nested':
+            val = stylish_deep(dict_level[key].get('value'), deep + 1)
             key = transform_key('unchanged', key)
-            result += f'{" " * (spaces + 2)}{key}: {val}\n'
-        elif dict_[key].get('type') == 'changed':
-            val_del, val_add = dict_[key].get("value")
-            val_del = formation_value(val_del, spaces + 4)
-            val_add = formation_value(val_add, spaces + 4)
-            key_del, key_add = transform_key(dict_[key].get('type'), key)
-            result += f'{" " * (spaces + 2)}{key_del}: ' \
+            result += f'{" " * (deep * 4 - 2)}{key}: {val}\n'
+        elif dict_level[key].get('type') == 'changed':
+            val_del, val_add = dict_level[key].get("value")
+            val_del = formation_value(val_del, deep + 1)
+            val_add = formation_value(val_add, deep + 1)
+            key_del, key_add = transform_key(dict_level[key].get('type'), key)
+            result += f'{" " * (deep * 4 - 2)}{key_del}: ' \
                       f'{val_del}\n'
-            result += f'{" " * (spaces + 2)}{key_add}: ' \
+            result += f'{" " * (deep * 4 - 2)}{key_add}: ' \
                       f'{val_add}\n'
         else:
-            value = dict_[key].get('value')
-            value = formation_value(value, spaces + 4)
-            key = transform_key(dict_[key].get('type'), key)
-            result += f'{" " * (spaces + 2)}{key}: {value}\n'
-    result += " " * spaces + '}'
+            value = dict_level[key].get('value')
+            value = formation_value(value, deep + 1)
+            key = transform_key(dict_level[key].get('type'), key)
+            result += f'{" " * (deep * 4 - 2)}{key}: {value}\n'
+    result += " " * (deep - 1) * 4 + '}'
     return result
+
+
+def stylish(dict_):
+    return stylish_deep(dict_, 1)
--- gendiff/generate_diff/parser.py
+++ gendiff/generate_diff/parser.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import json
 import yaml
+from typing import TextIO
 
 
-def parser(extension: str, val: str) -> dict:
+def parser(extension: str, open_file: TextIO) -> dict:
     if extension == '.json':
-        return json.load(val)
+        return json.load(open_file)
     if extension in ('.yaml', '.yml'):
-        return yaml.safe_load(val)
+        return yaml.safe_load(open_file)
