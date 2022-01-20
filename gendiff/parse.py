76bf3f09ba51b31af8258cba932cfda57eea02a4
Nella <nella611@yandex.ru>
2022-01-20 16:25:47 +0200

    changed parse signature

--- gendiff/parse.py
+++ gendiff/parse.py
@@ -2,8 +2,8 @@ import json
 import yaml
 
 
-def parse(extension: str, open_file: str) -> dict:
-    if extension == '.json':
-        return json.loads(open_file)
-    if extension in ('.yaml', '.yml'):
-        return yaml.safe_load(open_file)
+def parse(data: str, format: str) -> dict:
+    if data == '.json':
+        return json.loads(format)
+    if data in ('.yaml', '.yml'):
+        return yaml.safe_load(format)
